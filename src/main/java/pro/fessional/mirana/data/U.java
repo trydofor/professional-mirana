a83ef38de25bfb624fdbfee6dfacdf9156703366
trydofor <trydofor@gmail.com>
2021-03-20 21:10:17 +0800

    [🐵新增] Z.Or用于Either场合

--- src/main/java/pro/fessional/mirana/data/U.java
+++ src/main/java/pro/fessional/mirana/data/U.java
@@ -10,6 +10,14 @@ import java.util.Objects;
  */
 public interface U {
 
+    static <L, R> Or<L, R> l(L l) {
+        return new Or<>(l, null);
+    }
+
+    static <L, R> Or<L, R> r(R r) {
+        return new Or<>(null, r);
+    }
+
     static <T1, T2> Two<T1, T2> of(T1 t1, T2 t2) {
         return new Two<>(t1, t2);
     }
@@ -40,6 +48,52 @@ public interface U {
 
     static <T1, T2, T3, T4, T5, T6, T7, T8, T9> Nine<T1, T2, T3, T4, T5, T6, T7, T8, T9> of(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9) {
         return new Nine<>(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+    }
+
+    /**
+     * Either Left or Right
+     *
+     * @param <L> Left
+     * @param <R> Right
+     */
+    class Or<L, R> {
+        private final L l;
+        private final R r;
+
+        public Or(L l, R r) {
+            this.l = l;
+            this.r = r;
+        }
+
+        public L left() {
+            return l;
+        }
+
+        public R right() {
+            return r;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Or)) return false;
+            Or<?, ?> or = (Or<?, ?>) o;
+            return Objects.equals(l, or.l) &&
+                    Objects.equals(r, or.r);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(l, r);
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "l=" + l +
+                    " | r=" + r +
+                    '}';
+        }
     }
 
     class Two<T1, T2> {
