f348e9653dde4244d15113bf3eac86ea97db1af8
trydofor <trydofor@gmail.com>
2021-06-24 18:47:00 +0800

    [🐶调整] 使用Weak和Soft引用重做G和L

--- src/main/java/pro/fessional/mirana/anti/G.java
+++ src/main/java/pro/fessional/mirana/anti/G.java
@@ -1,45 +1,58 @@
 package pro.fessional.mirana.anti;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.WeakHashMap;
 
 /**
- * 反模式，不要过度使用: 主要用来debug或跨层传递
+ * 反模式，不要过度使用: 主要用来debug或跨层传递。
+ * 及时使用，及时移除。WeakReference，丢失Key则可能被GC
  *
  * @author trydofor
  * @since 2021-06-23
  */
 public class G {
 
-    private static final ConcurrentHashMap<Object, Object> GMap = new ConcurrentHashMap<>();
-    private static final ThreadLocal<Map<Object, Object>> LMap = ThreadLocal.withInitial(HashMap::new);
+    private static final WeakHashMap<Object, Object> GMap = new WeakHashMap<>();
+    private static final ThreadLocal<WeakHashMap<Object, Object>> LMap = new ThreadLocal<>();
 
     public static void globalPut(Object key, Object value) {
-        GMap.put(key, value);
+        synchronized (GMap) {
+            GMap.put(key, value);
+        }
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T globalGet(Object key) {
-        return (T) GMap.get(key);
+        synchronized (GMap) {
+            return (T) GMap.get(key);
+        }
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T globalDel(Object key) {
-        return (T) GMap.remove(key);
+        synchronized (GMap) {
+            return (T) GMap.remove(key);
+        }
     }
 
     public static void threadPut(Object key, Object value) {
-        LMap.get().put(key, value);
+        WeakHashMap<Object, Object> mp = LMap.get();
+        if (mp == null) {
+            mp = new WeakHashMap<>();
+            LMap.set(mp);
+        }
+
+        mp.put(key, value);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T threadGet(Object key) {
-        return (T) LMap.get().get(key);
+        final WeakHashMap<Object, Object> mp = LMap.get();
+        return mp == null ? null : (T) mp.get(key);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T threadDel(Object key) {
-        return (T) LMap.get().remove(key);
+        final WeakHashMap<Object, Object> mp = LMap.get();
+        return mp == null ? null : (T) mp.remove(key);
     }
 }
--- src/main/java/pro/fessional/mirana/anti/L.java
+++ src/main/java/pro/fessional/mirana/anti/L.java
@@ -5,11 +5,15 @@ import pro.fessional.mirana.data.Null;
 import pro.fessional.mirana.text.BuilderHolder;
 import pro.fessional.mirana.text.FormatUtil;
 
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * 反模式，不要过度使用: ThreadLocal收集信息，收集日志
+ * 反模式，不要过度使用: ThreadLocal收集信息，收集日志。
+ * 及时使用，及时移除。当内存不够是SoftReference会被GC。
+ * <p>
  * `(Cate:)?Message\n`格式的消息。
  * 最后一个`\n`可以省略，支持slf4j语法
  *
@@ -17,11 +21,12 @@ import java.util.List;
  * @since 2021-03-24
  */
 public class L {
-    public static final String CateTkn = ":";
-    public static final String LineTkn = "\n";
+
+    public static volatile String CateTkn = ":";
+    public static volatile String LineTkn = "\n";
 
     private static final BuilderHolder Builder = new BuilderHolder();
-    private static final ThreadLocal<List<D>> Holder = ThreadLocal.withInitial(ArrayList::new);
+    private static final ThreadLocal<SoftReference<List<D>>> Holder = new ThreadLocal<>();
 
     public static void error(String message) {
         error(message, Null.Objects);
@@ -56,12 +61,24 @@ public class L {
     }
 
     public static void message(String cate, String message, Object... args) {
-        Holder.get().add(new D(cate, message, args));
+        final D d = new D(cate, message, args);
+        final SoftReference<List<D>> ref = Holder.get();
+        if (ref != null) {
+            final List<D> ds = ref.get();
+            if (ds != null) {
+                ds.add(d);
+                return;
+            }
+        }
+
+        final List<D> ds = new ArrayList<>();
+        ds.add(d);
+        Holder.set(new SoftReference<>(ds));
     }
 
     @NotNull
     public static String finish() {
-        final List<D> ds = Holder.get();
+        final List<D> ds = inspect();
         final String rst = format(ds);
         ds.clear();
         return rst;
@@ -69,12 +86,19 @@ public class L {
 
     @NotNull
     public static List<D> inspect() {
-        return Holder.get();
+        final SoftReference<List<D>> ref = Holder.get();
+        if (ref == null) {
+            return Collections.emptyList();
+        }
+        else {
+            final List<D> ds = ref.get();
+            return ds == null ? Collections.emptyList() : ds;
+        }
     }
 
     @NotNull
     public static List<D> remove() {
-        final List<D> ds = Holder.get();
+        final List<D> ds = inspect();
         Holder.remove();
         return ds;
     }
