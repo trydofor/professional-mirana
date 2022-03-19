6f4413707b16c223a97a93a566fca1735fb9f177
trydofor <trydofor@gmail.com>
2022-03-19 16:37:49 +0800

    ✨ 新增notNull的else参数

--- src/main/java/pro/fessional/mirana/data/Null.java
+++ src/main/java/pro/fessional/mirana/data/Null.java
@@ -22,51 +22,30 @@ public class Null {
     public static final float Flt32 = 0.0F;
     public static final double Flt64 = 0.0D;
 
-    @NotNull
-    public static final Enum<?> Enm = Eu.Null;
-    @NotNull
-    public static final Class<?> Clz = Void.class;
+    public static final @NotNull Enum<?> Enm = Eu.Null;
+    public static final @NotNull Class<?> Clz = Void.class;
 
     //
-    @NotNull
-    public static final String Str = "";
-    @NotNull
-    public static final boolean[] Bools = new boolean[0];
-    @NotNull
-    public static final byte[] Bytes = new byte[0];
-    @NotNull
-    public static final char[] Chars = new char[0];
-    @NotNull
-    public static final short[] Shorts = new short[0];
-    @NotNull
-    public static final int[] Ints = new int[0];
-    @NotNull
-    public static final long[] Longs = new long[0];
-    @NotNull
-    public static final float[] Floats = new float[0];
-    @NotNull
-    public static final double[] Doubles = new double[0];
-    @NotNull
-    public static final Object[] Objects = new Object[0];
-    @NotNull
-    public static final BigDecimal[] BigDecimals = new BigDecimal[0];
+    public static final @NotNull String Str = "";
+    public static final boolean @NotNull [] Bools = new boolean[0];
+    public static final byte @NotNull [] Bytes = new byte[0];
+    public static final char @NotNull [] Chars = new char[0];
+    public static final short @NotNull [] Shorts = new short[0];
+    public static final int @NotNull [] Ints = new int[0];
+    public static final long @NotNull [] Longs = new long[0];
+    public static final float @NotNull [] Floats = new float[0];
+    public static final double @NotNull [] Doubles = new double[0];
+    public static final Object @NotNull [] Objects = new Object[0];
+    public static final BigDecimal @NotNull [] BigDecimals = new BigDecimal[0];
 
-    @NotNull
-    public static final Boolean[] BoolArr = new Boolean[0];
-    @NotNull
-    public static final Byte[] ByteArr = new Byte[0];
-    @NotNull
-    public static final Short[] ShortArr = new Short[0];
-    @NotNull
-    public static final Integer[] IntArr = new Integer[0];
-    @NotNull
-    public static final Long[] LongArr = new Long[0];
-    @NotNull
-    public static final Float[] FloatArr = new Float[0];
-    @NotNull
-    public static final Double[] DoubleArr = new Double[0];
-    @NotNull
-    public static final String[] StrArr = new String[0];
+    public static final Boolean @NotNull [] BoolArr = new Boolean[0];
+    public static final Byte @NotNull [] ByteArr = new Byte[0];
+    public static final Short @NotNull [] ShortArr = new Short[0];
+    public static final Integer @NotNull [] IntArr = new Integer[0];
+    public static final Long @NotNull [] LongArr = new Long[0];
+    public static final Float @NotNull [] FloatArr = new Float[0];
+    public static final Double @NotNull [] DoubleArr = new Double[0];
+    public static final String @NotNull [] StrArr = new String[0];
 
     // /////////////////
     public static boolean asNull(boolean v) {
@@ -137,7 +116,6 @@ public class Null {
         return v == null || v.length() == 0;
     }
 
-
     public static boolean asNull(boolean[] v) {
         return v == null || v.length == 0;
     }
@@ -182,103 +160,167 @@ public class Null {
         return v == null || v == Clz;
     }
 
-    @NotNull
-    public static Boolean notNull(Boolean v) {
+    // /////////////////
+
+    public static boolean notNull(Boolean v) {
         return v == null ? Int01 : v;
     }
 
-    @NotNull
-    public static Byte notNull(Byte v) {
+    public static byte notNull(Byte v) {
         return v == null ? Int08 : v;
     }
 
-    @NotNull
-    public static Character notNull(Character v) {
+    public static char notNull(Character v) {
         return v == null ? Int16 : v;
     }
 
-    @NotNull
-    public static Short notNull(Short v) {
+    public static short notNull(Short v) {
         return v == null ? (short) Int16 : v;
     }
 
-    @NotNull
-    public static Integer notNull(Integer v) {
+    public static int notNull(Integer v) {
         return v == null ? Int32 : v;
     }
 
-    @NotNull
-    public static Long notNull(Long v) {
+    public static long notNull(Long v) {
         return v == null ? Int64 : v;
     }
 
-    @NotNull
-    public static Float notNull(Float v) {
+    public static float notNull(Float v) {
         return v == null ? Flt32 : v;
     }
 
-    @NotNull
-    public static Double notNull(Double v) {
+    public static double notNull(Double v) {
         return v == null ? Flt64 : v;
     }
 
-    @NotNull
-    public static String notNull(CharSequence v) {
+    public static @NotNull String notNull(CharSequence v) {
         return v == null ? Str : v.toString();
     }
 
-    @NotNull
-    public static boolean[] notNull(boolean[] v) {
+    public static boolean @NotNull [] notNull(boolean[] v) {
         return v == null ? Bools : v;
     }
 
-    @NotNull
-    public static byte[] notNull(byte[] v) {
+    public static byte @NotNull [] notNull(byte[] v) {
         return v == null ? Bytes : v;
     }
 
-    @NotNull
-    public static char[] notNull(char[] v) {
+    public static char @NotNull [] notNull(char[] v) {
         return v == null ? Chars : v;
     }
 
-    @NotNull
-    public static short[] notNull(short[] v) {
+    public static short @NotNull [] notNull(short[] v) {
         return v == null ? Shorts : v;
     }
 
-    @NotNull
-    public static int[] notNull(int[] v) {
+    public static int @NotNull [] notNull(int[] v) {
         return v == null ? Ints : v;
     }
 
-    @NotNull
-    public static long[] notNull(long[] v) {
+    public static long @NotNull [] notNull(long[] v) {
         return v == null ? Longs : v;
     }
 
-    @NotNull
-    public static float[] notNull(float[] v) {
+    public static float @NotNull [] notNull(float[] v) {
         return v == null ? Floats : v;
     }
 
-    @NotNull
-    public static double[] notNull(double[] v) {
+    public static double @NotNull [] notNull(double[] v) {
         return v == null ? Doubles : v;
     }
 
-    @NotNull
-    public static Object[] notNull(Object[] v) {
+    public static Object @NotNull [] notNull(Object[] v) {
         return v == null ? Objects : v;
     }
 
-    @NotNull
-    public static Enum<?> notNull(Enum<?> v) {
+    public static @NotNull Enum<?> notNull(Enum<?> v) {
         return v == null ? Enm : v;
     }
 
-    @NotNull
-    public static Class<?> notNull(Class<?> v) {
+    public static @NotNull Class<?> notNull(Class<?> v) {
         return v == null ? Clz : v;
+    }
+
+    // /////////////////
+
+    public static boolean notNull(Boolean v, boolean e) {
+        return v == null ? e : v;
+    }
+
+    public static byte notNull(Byte v, byte e) {
+        return v == null ? e : v;
+    }
+
+    public static char notNull(Character v, char e) {
+        return v == null ? e : v;
+    }
+
+    public static short notNull(Short v, short e) {
+        return v == null ? e : v;
+    }
+
+    public static int notNull(Integer v, int e) {
+        return v == null ? e : v;
+    }
+
+    public static long notNull(Long v, long e) {
+        return v == null ? e : v;
+    }
+
+    public static float notNull(Float v, float e) {
+        return v == null ? e : v;
+    }
+
+    public static double notNull(Double v, double e) {
+        return v == null ? e : v;
+    }
+
+    public static @NotNull String notNull(CharSequence v, @NotNull String e) {
+        return v == null ? e : v.toString();
+    }
+
+    public static boolean @NotNull [] notNull(boolean[] v, boolean @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static byte @NotNull [] notNull(byte[] v, byte @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static char @NotNull [] notNull(char[] v, char @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static short @NotNull [] notNull(short[] v, short @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static int @NotNull [] notNull(int[] v, int @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static long @NotNull [] notNull(long[] v, long @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static float @NotNull [] notNull(float[] v, float @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static double @NotNull [] notNull(double[] v, double @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static Object @NotNull [] notNull(Object[] v, Object @NotNull [] e) {
+        return v == null ? e : v;
+    }
+
+    public static <T extends Enum<T>> @NotNull T notNull(T v, T e) {
+        return v == null ? e : v;
+    }
+
+    public static <T> @NotNull Class<T> notNull(Class<T> v, Class<T> e) {
+        return v == null ? e : v;
     }
 }
