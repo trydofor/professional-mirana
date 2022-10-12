2a96e398dded18b5ce2e040d3ea42907e886125f
trydofor <trydofor@gmail.com>
2022-10-12 11:44:37 +0800

    ✨ 增加简单的ULID实现

---
+++ src/main/java/pro/fessional/mirana/id/Ulid.java
@@ -0,0 +1,99 @@
+package pro.fessional.mirana.id;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 仅简单生成[ULID](https://github.com/ulid/spec)
+ * 重度使用，推荐 https://github.com/huxi/sulky/tree/master/sulky-ulid
+ *
+ * @author trydofor
+ * @since 2022-10-12
+ */
+public class Ulid {
+
+    @NotNull
+    public static String next() {
+        return next(System.currentTimeMillis(), ThreadLocalRandom.current());
+    }
+
+    @NotNull
+    public static String next(Random random) {
+        return next(System.currentTimeMillis(), random);
+    }
+
+    @NotNull
+    public static String next(long time) {
+        return next(time, ThreadLocalRandom.current());
+    }
+
+    @NotNull
+    public static String next(long time, Random random) {
+        StringBuilder buff = new StringBuilder(26);
+        next(buff, time, random);
+        return buff.toString();
+    }
+
+    //
+    public static void next(@NotNull StringBuilder buff) {
+        next(buff, System.currentTimeMillis(), ThreadLocalRandom.current());
+    }
+
+
+    public static void next(@NotNull StringBuilder buff, Random random) {
+        next(buff, System.currentTimeMillis(), random);
+    }
+
+    public static void next(@NotNull StringBuilder buff, long time) {
+        next(buff, time, ThreadLocalRandom.current());
+    }
+
+    private static final char[] CHARS = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+            'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X',
+            'Y', 'Z',
+            };
+
+    public static void next(@NotNull StringBuilder buff, long time, Random random) {
+        if ((time & 0xFFFF_0000_0000_0000L) != 0) {
+            throw new IllegalArgumentException("UTC timestamp must before 10889-08-02T05:31:50.655Z");
+        }
+
+        final byte[] bits80 = new byte[10];
+        random.nextBytes(bits80);
+
+        //// time 48 = 3+9*5
+        buff.append(CHARS[((int) (time >>> 45)) & 0x1F]);
+        buff.append(CHARS[((int) (time >>> 40)) & 0x1F]);
+        buff.append(CHARS[((int) (time >>> 35)) & 0x1F]);
+        buff.append(CHARS[((int) (time >>> 30)) & 0x1F]);
+        buff.append(CHARS[((int) (time >>> 25)) & 0x1F]);
+        buff.append(CHARS[((int) (time >>> 20)) & 0x1F]);
+        buff.append(CHARS[((int) (time >>> 15)) & 0x1F]);
+        buff.append(CHARS[((int) (time >>> 10)) & 0x1F]);
+        buff.append(CHARS[((int) (time >>> 5)) & 0x1F]);
+        buff.append(CHARS[((int) (time)) & 0x1F]);
+
+        //// bits 80 = 16*5
+        buff.append(CHARS[((bits80[0] & 0xFF) >>> 3)]);
+        buff.append(CHARS[(((bits80[0] << 2) | ((bits80[1] & 0xFF) >>> 6)) & 0x1F)]);
+        buff.append(CHARS[(((bits80[1] & 0xFF) >>> 1) & 0x1F)]);
+        buff.append(CHARS[(((bits80[1] << 4) | ((bits80[2] & 0xFF) >>> 4)) & 0x1F)]);
+        buff.append(CHARS[(((bits80[2] << 1) | ((bits80[3] & 0xFF) >>> 7)) & 0x1F)]);
+        buff.append(CHARS[(((bits80[3] & 0xFF) >>> 2) & 0x1F)]);
+        buff.append(CHARS[(((bits80[3] << 3) | ((bits80[4] & 0xFF) >>> 5)) & 0x1F)]);
+        buff.append(CHARS[(bits80[4] & 0x1F)]);
+
+        buff.append(CHARS[((bits80[5] & 0xFF) >>> 3)]);
+        buff.append(CHARS[(((bits80[5] << 2) | ((bits80[6] & 0xFF) >>> 6)) & 0x1F)]);
+        buff.append(CHARS[(((bits80[6] & 0xFF) >>> 1) & 0x1F)]);
+        buff.append(CHARS[(((bits80[6] << 4) | ((bits80[7] & 0xFF) >>> 4)) & 0x1F)]);
+        buff.append(CHARS[(((bits80[7] << 1) | ((bits80[8] & 0xFF) >>> 7)) & 0x1F)]);
+        buff.append(CHARS[(((bits80[8] & 0xFF) >>> 2) & 0x1F)]);
+        buff.append(CHARS[(((bits80[8] << 3) | ((bits80[9] & 0xFF) >>> 5)) & 0x1F)]);
+        buff.append(CHARS[(bits80[9] & 0x1F)]);
+    }
+}
---
+++ src/test/java/pro/fessional/mirana/id/UlidTest.java
@@ -0,0 +1,34 @@
+package pro.fessional.mirana.id;
+
+import de.huxhorn.sulky.ulid.ULID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author trydofor
+ * @since 2022-10-12
+ */
+class UlidTest {
+
+    @Test
+    void next() {
+        /*
+         01GF53QXEQ5KV895FQMPCWG0RC
+         01GF53QXEQP3EHECQ52ZTXJB23
+         01GF53QXEQ91S443Z1BAWBX2G9
+         01GF53QXEQZWJ5BCFXJM6V4MH1
+         01GF53QXEQRXXC2C3NWKWQHAEK
+         01GF53QXERPY037QK1J6VCBNJV
+         */
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Ulid.next());
+
+        }
+
+        for (int i = 0; i < 100_000; i++) {
+            final String id = Ulid.next();
+            final ULID.Value uv = ULID.parseULID(id);
+            Assertions.assertEquals(id, uv.toString());
+        }
+    }
+}
