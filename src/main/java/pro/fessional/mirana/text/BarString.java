20d3818f2be0a9602acbba43c0433cbbfbf04b94
trydofor <trydofor@gmail.com>
2022-11-10 17:59:38 +0800

    ✨ 增加BarString，简单的分隔字符串

---
+++ src/main/java/pro/fessional/mirana/text/BarString.java
@@ -0,0 +1,237 @@
+package pro.fessional.mirana.text;
+
+import org.jetbrains.annotations.NotNull;
+import pro.fessional.mirana.data.Null;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * <pre>
+ * 构造Bar分隔的字符串，首尾一定有Bar，Bar由以下字符自动调整。
+ * https://en.wikipedia.org/wiki/Vertical_bar
+ * U+007C | VERTICAL LINE (&verbar;, &vert;, &VerticalLine;) (single vertical line)
+ * U+00A6 ¦ BROKEN BAR (&brvbar;) (single broken line)
+ * U+2016 ‖ DOUBLE VERTICAL LINE (&Verbar;, &Vert;) (double vertical line ( {\displaystyle \|}\| ): used in pairs to indicate norm)
+ * U+FF5C ｜ FULLWIDTH VERTICAL LINE (Fullwidth form)
+ * U+2225 ∥ PARALLEL TO (&DoubleVerticalBar;, &par;, &parallel;, &shortparallel;, &spar;)
+ * U+01C0 ǀ LATIN LETTER DENTAL CLICK
+ * U+01C1 ǁ LATIN LETTER LATERAL CLICK
+ * U+2223 ∣ DIVIDES (&mid;, &shortmid;, &smid;, &VerticalBar;)
+ * U+2502 │ BOX DRAWINGS LIGHT VERTICAL (&boxv;) (and various other box drawing characters in the range U+2500 to U+257F)
+ * U+0964 । DEVANAGARI DANDA
+ * U+0965 ॥ DEVANAGARI DOUBLE DANDA
+ * </pre>
+ *
+ * @author trydofor
+ * @since 2022-11-08
+ */
+public class BarString {
+    public static final char SingleBar = '|';
+    public static final char BrokenBar = '¦';
+    public static final char FullwidthBar = '｜';
+    public static final char DentalBar = 'ǀ';
+    public static final char DividesBar = '∣';
+    public static final char DrawingBar = '│';
+    public static final char DandaBar = '।';
+    public static final char DoubleBar = '‖';
+    public static final char ParallelBar = '∥';
+    public static final char LateralBar = 'ǁ';
+    public static final char DoubleDandaBar = '॥';
+
+    private static final char[] Bars = {
+            SingleBar, // '|'
+            BrokenBar, // '¦'
+            FullwidthBar, // '｜'
+            DentalBar, // 'ǀ'
+            DividesBar, // '∣'
+            DrawingBar, // '│'
+            DandaBar, // '।'
+            DoubleBar, // '‖'
+            ParallelBar, // '∥'
+            LateralBar, // 'ǁ'
+            DoubleDandaBar, // '॥'
+    };
+
+    private int index = 0;
+    private boolean valid = true;
+    private int count = 0;
+
+    private final LinkedList<Ent> values = new LinkedList<>();
+    private final boolean[] exists = new boolean[Bars.length];
+
+    public BarString() {
+    }
+
+    private void appendKey() {
+        values.add(Ent.Bar);
+    }
+
+    private void appendValue(@NotNull String str, boolean check) {
+        count += str.length();
+        values.add(new Ent(str, check));
+
+        if (check && valid && str.indexOf(Bars[index]) >= 0) {
+            exists[index] = true;
+            boolean vd = false;
+            for (int i = 0; i < exists.length; i++) {
+                if (exists[i]) continue;
+
+                for (Ent o : values) {
+                    if (o != Ent.Bar && o.check) {
+//                        System.out.println(str + ">>>" + o.value + ">>>" + Bars[i] + "=" + exists[i]);
+                        if (o.value.indexOf(Bars[i]) >= 0) {
+                            exists[i] = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!exists[i]) {
+                    index = i;
+                    vd = true;
+                    break;
+                }
+            }
+
+            valid = vd;
+        }
+    }
+
+    public void append(Object obj) {
+        appendKey();
+        if (obj != null) {
+            appendValue(obj.toString(), true);
+        }
+    }
+
+    public void append(BigDecimal num) {
+        appendKey();
+        if (num != null) {
+            appendValue(num.toPlainString(), false);
+        }
+    }
+
+    public void append(long num) {
+        appendKey();
+        appendValue(String.valueOf(num), false);
+    }
+
+    public void append(int num) {
+        appendKey();
+        appendValue(String.valueOf(num), false);
+    }
+
+    public void append(char num) {
+        appendKey();
+        appendValue(String.valueOf(num), true);
+    }
+
+    public void append(boolean num) {
+        appendKey();
+        appendValue(String.valueOf(num), false);
+    }
+
+    public void append(double num) {
+        appendKey();
+        appendValue(String.valueOf(num), false);
+    }
+
+    public void append(float num) {
+        appendKey();
+        appendValue(String.valueOf(num), false);
+    }
+
+
+    /**
+     * 字符串中是否包含了指定的Bar
+     */
+    public boolean isValid() {
+        return valid;
+    }
+
+    @Override
+    public String toString() {
+        if (values.isEmpty()) return Null.Str;
+
+        final char bar = Bars[index];
+        StringBuilder buff = new StringBuilder(count + values.size());
+        for (Ent v : values) {
+            if (v == Ent.Bar) {
+                buff.append(bar);
+            }
+            else {
+                buff.append(v.value);
+            }
+        }
+        buff.append(bar);
+        return buff.toString();
+    }
+
+    private static class Ent {
+        public static final Ent Bar = new Ent(Null.Str, false);
+        @NotNull
+        private final String value;
+        private final boolean check;
+
+        public Ent(@NotNull String value, boolean check) {
+            this.value = value;
+            this.check = check;
+        }
+    }
+
+
+    @NotNull
+    public static ArrayList<String> split(String str) {
+        return split(str, -1, false);
+    }
+
+    @NotNull
+    public static ArrayList<String> split(String str, int max) {
+        return split(str, max, false);
+    }
+
+    /**
+     * 解析一个BarString，数组内元素非null
+     *
+     * @param str     BarString
+     * @param max     最大数量, 小于0表示无限制
+     * @param exactly 是否必须为max
+     */
+    @NotNull
+    public static ArrayList<String> split(String str, int max, boolean exactly) {
+        final ArrayList<String> list = new ArrayList<>(max > 0 ? max : 8);
+        if (str == null || max == 0) return list;
+
+        final int len = str.length();
+        if (len <= 1) return list;
+
+        char bar = str.charAt(0);
+        if (bar != str.charAt(len - 1)) {
+            if (exactly && max != 1) {
+                list.add(str);
+            }
+            return list;
+        }
+
+        int off = 1, pls = 1;
+        for (int cur = str.indexOf(bar, off); cur >= off; cur = str.indexOf(bar, off)) {
+            if (pls == max) {
+                list.add(str.substring(off, len - 1));
+                break;
+            }
+            else {
+                pls++;
+                list.add(str.substring(off, cur));
+                off = cur + 1;
+            }
+        }
+
+        if (exactly && max != list.size()) {
+            list.clear();
+        }
+
+        return list;
+    }
+}
---
+++ src/test/java/pro/fessional/mirana/text/BarStringTest.java
@@ -0,0 +1,179 @@
+package pro.fessional.mirana.text;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * @author trydofor
+ * @since 2022-11-08
+ */
+class BarStringTest {
+
+    @Test
+    void split0() {
+        BarString bs = new BarString();
+        final String str = bs.toString();
+        final ArrayList<String> split = BarString.split(str, -1, false);
+        Assertions.assertEquals("", str);
+        Assertions.assertEquals(0, split.size());
+    }
+
+    @Test
+    void split1() {
+        BarString bs = new BarString();
+        bs.append(123);
+        final String str = bs.toString();
+        final ArrayList<String> split = BarString.split(str, -1, false);
+        Assertions.assertEquals("|123|", str);
+        Assertions.assertEquals(1, split.size());
+        Assertions.assertEquals("123", split.get(0));
+
+        BarString bs0 = new BarString();
+        bs0.append(null);
+        final String str0 = bs0.toString();
+        final ArrayList<String> split0 = BarString.split(str0, -1, false);
+        Assertions.assertEquals("||", str0);
+        Assertions.assertEquals(1, split0.size());
+        Assertions.assertEquals("", split0.get(0));
+    }
+
+    @Test
+    void split2() {
+        BarString bs = new BarString();
+        bs.append(null);
+        bs.append(123);
+        final String str = bs.toString();
+        final ArrayList<String> split = BarString.split(str, -1, false);
+        Assertions.assertEquals("||123|", str);
+        Assertions.assertEquals(2, split.size());
+        Assertions.assertEquals("", split.get(0));
+        Assertions.assertEquals("123", split.get(1));
+
+        BarString bs0 = new BarString();
+        bs0.append(null);
+        bs0.append(null);
+        final String str0 = bs0.toString();
+        final ArrayList<String> split0 = BarString.split(str0, -1, false);
+        Assertions.assertEquals("|||", str0);
+        Assertions.assertEquals(2, split0.size());
+        Assertions.assertEquals("", split0.get(0));
+        Assertions.assertEquals("", split0.get(1));
+
+        BarString bs3 = new BarString();
+        bs3.append(null);
+        bs3.append(null);
+        final String str3 = bs3.toString();
+        final ArrayList<String> split3 = BarString.split(str3, 0, false);
+        Assertions.assertEquals("|||", str3);
+        Assertions.assertEquals(0, split3.size());
+
+        BarString bs4 = new BarString();
+        bs4.append(null);
+        bs4.append(null);
+        final String str4 = bs4.toString();
+        final ArrayList<String> split4 = BarString.split(str4, 1, false);
+        Assertions.assertEquals("|||", str4);
+        Assertions.assertEquals(1, split4.size());
+        Assertions.assertEquals("|", split4.get(0));
+    }
+
+    @Test
+    void split6() {
+        BarString bs = new BarString();
+        bs.append(123);
+        bs.append(new BigDecimal("45.67"));
+        bs.append(null);
+        bs.append(null);
+        bs.append(null);
+        bs.append(true);
+        final String str = bs.toString();
+        Assertions.assertEquals("|123|45.67||||true|", str);
+        Assertions.assertTrue(bs.isValid());
+
+        final ArrayList<String> split = BarString.split(str, 6, true);
+        Assertions.assertEquals(6, split.size());
+        Assertions.assertEquals("123", split.get(0));
+        Assertions.assertEquals("45.67", split.get(1));
+        Assertions.assertEquals("", split.get(2));
+        Assertions.assertEquals("", split.get(3));
+        Assertions.assertEquals("", split.get(4));
+        Assertions.assertEquals("true", split.get(5));
+
+        final ArrayList<String> pt2 = BarString.split(str, 5, false);
+        Assertions.assertEquals(5, pt2.size());
+        Assertions.assertEquals("123", pt2.get(0));
+        Assertions.assertEquals("45.67", pt2.get(1));
+        Assertions.assertEquals("", pt2.get(2));
+        Assertions.assertEquals("", pt2.get(3));
+        Assertions.assertEquals("|true", pt2.get(4));
+    }
+
+    @Test
+    void splitX() {
+        BarString bs = new BarString();
+        bs.append('|');
+        bs.append('¦');
+        bs.append('‖');
+        bs.append('｜');
+        bs.append('∥');
+        bs.append('ǀ');
+        bs.append('ǁ');
+        bs.append('∣');
+        bs.append('│');
+        bs.append('।');
+        // bs.append("॥");
+        final String str = bs.toString();
+        Assertions.assertEquals("॥|॥¦॥‖॥｜॥∥॥ǀ॥ǁ॥∣॥│॥।॥", str);
+        Assertions.assertTrue(bs.isValid());
+        final ArrayList<String> split = BarString.split(str);
+        Assertions.assertEquals(10, split.size());
+        Assertions.assertEquals("|", split.get(0));
+        Assertions.assertEquals("¦", split.get(1));
+        Assertions.assertEquals("‖", split.get(2));
+        Assertions.assertEquals("｜", split.get(3));
+        Assertions.assertEquals("∥", split.get(4));
+        Assertions.assertEquals("ǀ", split.get(5));
+        Assertions.assertEquals("ǁ", split.get(6));
+        Assertions.assertEquals("∣", split.get(7));
+        Assertions.assertEquals("│", split.get(8));
+        Assertions.assertEquals("।", split.get(9));
+    }
+
+    @Test
+    void splitY() {
+        BarString bs = new BarString();
+        bs.append('|');
+        bs.append('¦');
+        bs.append('‖');
+        bs.append('｜');
+        bs.append('∥');
+        bs.append('ǀ');
+        bs.append('ǁ');
+        bs.append('∣');
+        bs.append('│');
+        bs.append('।');
+        bs.append('॥');
+        final String str = bs.toString();
+        Assertions.assertEquals("॥|॥¦॥‖॥｜॥∥॥ǀ॥ǁ॥∣॥│॥।॥॥॥", str);
+        Assertions.assertFalse(bs.isValid());
+
+        final ArrayList<String> split = BarString.split(str, 11);
+        final Iterator<String> it = split.iterator();
+        Assertions.assertEquals(11, split.size());
+        Assertions.assertEquals("|", it.next());
+        Assertions.assertEquals("¦", it.next());
+        Assertions.assertEquals("‖", it.next());
+        Assertions.assertEquals("｜", it.next());
+        Assertions.assertEquals("∥", it.next());
+        Assertions.assertEquals("ǀ", it.next());
+        Assertions.assertEquals("ǁ", it.next());
+        Assertions.assertEquals("∣", it.next());
+        Assertions.assertEquals("│", it.next());
+        Assertions.assertEquals("।", it.next());
+        Assertions.assertEquals("॥", it.next());
+    }
+}
